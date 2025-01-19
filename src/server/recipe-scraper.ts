9ad4e0943284072eb711052a474559015a60638c
David Mitchell <dmitchek@gmail.com>
2025-01-19 15:46:34 -0500

    Test Chromium package

--- src/server/recipe-scraper.ts
+++ src/server/recipe-scraper.ts
@@ -1,12 +1,31 @@
 import * as cheerio from "cheerio";
 import puppeteer, { type LaunchOptions } from "puppeteer";
 
+import chromium from "chrome-aws-lambda";
+
 export async function getRecipeHtmlFromUrl(url: string, plugin?: string) {
-  const browser = await puppeteer.launch({
-    timeout: 0,
-    headless: true,
-    args: ["--no-sandbox", "--disable-gpu"],
-  } as LaunchOptions);
+  //let browser;
+  // if (process.env.NODE_ENV === "production") {
+  //   browser = await chromium.puppeteer.launch({
+  //     args: chromium.args,
+  //     defaultViewport: chromium.defaultViewport,
+  //     executablePath: await chromium.executablePath,
+  //     headless: chromium.headless,
+  //   });
+  // } else {
+  //   browser = await puppeteer.launch({
+  //     timeout: 0,
+  //     headless: true,
+  //     args: ["--no-sandbox", "--disable-gpu"],
+  //   } as LaunchOptions);
+  // }
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
 
   const defaultSelector =
     "div[classs^=tasty-recipes], div[class^=wprm], div[class^=recipe-container]";
